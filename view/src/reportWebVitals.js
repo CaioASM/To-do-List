0aedd404f54067fefce43d4e8cc23e601c7b6574
CaioASM <caiomiranda8@gmail.com>
2023-12-14 14:04:06 -0800

    'second wave of changes'

--- view/src/reportWebVitals.js
+++ view/src/reportWebVitals.js
@@ -1,6 +1,8 @@
-const reportWebVitals = onPerfEntry => {
+const reportWebVitals = (onPerfEntry) => { // Added parentheses around onPerfEntry
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    import('web-vitals').then(({
+      getCLS, getFID, getFCP, getLCP, getTTFB, // Added line breaks as per ESLint rules
+    }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
